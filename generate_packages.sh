cb9afa44fc9ae8b374a4d4ed3c1f6815116583bb
axs6 <axs6@users.noreply.github.com>
2024-06-12 06:16:21 +0000

    deploy: 117fc83801dd9bf5d14441aa3332afcf8d8b8398

--- generate_packages.sh
+++ generate_packages.sh
@@ -25,11 +25,11 @@ gzip -fk Packages
 
 # Create the Release file
 cat <<EOF > Release
-Origin: 💫A先生✨
-Label: 💫A先生✨
+Origin: 💫Axs pro✨
+Label: 💫Axs pro✨
 Suite: stable
 Version: 1.0
-Codename: A先生
+Codename: Axs pro
 Architectures: iphoneos-arm64 iphoneos-arm64e
 Components: main
 Description: 自用插件分享，有问题请卸载！！！
